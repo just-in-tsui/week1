8bc040d39d25812a5c849b5f5b1998367a22483f
just-in-tsui <justintc2002@gmail.com>
2022-05-09 23:12:54 +0800

    9/5

--- Q2/scripts/compile-Multiplier3-plonk.sh
+++ Q2/scripts/compile-Multiplier3-plonk.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
 # [assignment] create your own bash script to compile Multipler3.circom using PLONK below
+cd contracts/circuits
+
+mkdir Multiplier3_plonk
+
+if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
+    echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
+else
+    echo 'Downloading powersOfTau28_hez_final_10.ptau'
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_10.ptau
+fi
+
+echo "Compiling Multiplier3.circom..."
+
+# compile circuit
+
+circom Multiplier3.circom --r1cs --wasm --sym -o Multiplier3_plonk
+snarkjs r1cs info Multiplier3_plonk/Multiplier3.r1cs
+snarkjs r1cs print Multiplier3_plonk/Multiplier3.r1cs Multiplier3_plonk/Multiplier3.sym
+
+# Start a new zkey and make a contribution
+snarkjs r1cs export json Multiplier3_plonk/Multiplier3.r1cs Multiplier3_plonk/Multiplier3.r1cs.json
+cat Multiplier3_plonk/Multiplier3.r1cs.json
+node Multiplier3_plonk/Multiplier3_js/generate_witness.js Multiplier3_plonk/Multiplier3_js/Multiplier3.wasm Multiplier3_plonk/input.json Multiplier3_plonk/witness.wtns
+
+
+snarkjs plonk setup Multiplier3_plonk/Multiplier3.r1cs powersOfTau28_hez_final_10.ptau Multiplier3_plonk/circuit_final.zkey
+
+snarkjs plonk prove Multiplier3_plonk/circuit_final.zkey Multiplier3_plonk/witness.wtns Multiplier3_plonk/proof.json Multiplier3_plonk/public.json
+snarkjs plonk verify Multiplier3_plonk/verification_key.json Multiplier3_plonk/public.json Multiplier3_plonk/proof.json
+
+snarkjs zkey export verificationkey Multiplier3_plonk/circuit_final.zkey Multiplier3_plonk/verification_key.json
+
+# generate solidity contract
+snarkjs zkey export solidityverifier Multiplier3_plonk/circuit_final.zkey ../Multiplier3Verifier_plonk.sol
+snarkjs zkey export soliditycalldata Multiplier3_plonk/public.json Multiplier3_plonk/proof.json
+
+
+
+
+cd ../..
--- Q2/test/test.js
+++ Q2/test/test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
-const { groth16 } = require("snarkjs");
+const { groth16, plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -35,22 +35,23 @@ describe("HelloWorld", function () {
 
     it("Should return true for correct proof", async function () {
         //[assignment] Add comments to explain what each line is doing
+        // Call the full prove function, set input signals a,b to 1,2
         const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2"}, "contracts/circuits/HelloWorld/HelloWorld_js/HelloWorld.wasm","contracts/circuits/HelloWorld/circuit_final.zkey");
-
+        // Print output signal c
         console.log('1x2 =',publicSignals[0]);
 
-        const editedPublicSignals = unstringifyBigInts(publicSignals);
-        const editedProof = unstringifyBigInts(proof);
-        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // Transform the string into JS valid value 
+        const editedProof = unstringifyBigInts(proof); // Transform the proof into JS valid value 
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // Call proof and public signals to export
     
-        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
-    
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // Replace characters  with empty string splitted by ','
+        // It also create a array with elments of x into Big Int type and to string
         const a = [argv[0], argv[1]];
         const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
-        const c = [argv[6], argv[7]];
-        const Input = argv.slice(8);
+        const c = [argv[6], argv[7]]; // Setting a,b,c from argv elements
+        const Input = argv.slice(8); 
 
-        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true; // Check whether output of .verifyProof is true.
     });
     it("Should return false for invalid proof", async function () {
         let a = [0, 0];
@@ -66,27 +67,71 @@ describe("Multiplier3 with Groth16", function () {
 
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const { proof, publicSignals } = await groth16.fullProve({"a":"1","b":"2","c":"3"}, "contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey");
+        // Print output signal c
+        console.log('1x2x3 =',publicSignals[0]);
+
+        const editedPublicSignals = unstringifyBigInts(publicSignals); // Transform the string into JS valid value 
+        const editedProof = unstringifyBigInts(proof); // Transform the proof into JS valid value 
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals); // Call proof and public signals to export
+    
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString()); // Replace characters  with empty string splitted by ','
+        // It also create a array with elments of x into Big Int type and to string
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]]; // Setting a,b,c from argv elements
+        const Input = argv.slice(8); 
+
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
 });
 
 
 describe("Multiplier3 with PLONK", function () {
+    let Verifier;
+    let verifier;
 
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("PlonkVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+
+        const { proof, publicSignals } = await plonk.prove("contracts/circuits/Multiplier3_plonk/circuit_final.zkey", "contracts/circuits/Multiplier3_plonk/witness.wtns");
+        // Print output signal c
+        const calldata = await plonk.exportSolidityCallData(unstringifyBigInts(proof), unstringifyBigInts(publicSignals))
+        console.log('1x2x3 =',publicSignals[0]);
+        const calldataSplit = calldata.split(',')
+        const proofFormatted = calldataSplit[0]
+        const publicSignalsFormatted = JSON.parse(calldataSplit[1]).map(x => BigInt(x).toString())
+        
+        // It also create a array with elments of x into Big Int type and to string
+
+        expect(await verifier.verifyProof(proofFormatted, publicSignalsFormatted)).to.be.true;
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let a = '0x00';
+        let b = ['0'];
+        expect(await verifier.verifyProof(a, b)).to.be.false;
     });
 });
